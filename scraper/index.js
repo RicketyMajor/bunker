f50e6ed716dc937479707d5e6d725ba02d26e4c5
Alonso Vera <alonsoveralarach@gmail.com>
2026-03-17 12:31:58 -0300

    feat: initialize Node.js scraper microservice within docker-compose ecosystem

---
+++ scraper/index.js
@@ -0,0 +1,6 @@
+console.log("🚀 Iniciando Microservicio Scraper de Novedades...");
+
+// Aquí irá la lógica de extracción más adelante
+setTimeout(() => {
+    console.log("✅ Servicio funcionando correctamente.");
+}, 1000);
